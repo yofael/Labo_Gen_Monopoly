8cf685a2da6f6e3278ec8e386dc2ae12858c368c
yoannsim <yoann.simonet@heig-vd.ch>
2019-04-12 11:21:35 +0200

    add board and tests

---
+++ src/test/java/board/BoardTest.java
@@ -0,0 +1,34 @@
+package board;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    Board testBoard;
+    private static final Logger LOG = Logger.getLogger(BoardTest.class.getName());
+    @BeforeEach
+    void setUp() {
+        testBoard = new Board();
+    }
+
+
+    @Test
+    void getSquareTest() {
+       for(int i=1;i<40;i++) {
+           Square old = new Square(Integer.toString(i));
+           Square s = testBoard.getSquare(old,12);
+           LOG.info(s.getName());
+           assertEquals(s.getName(),Integer.toString((((i + 12))>40)?((i + 12)%41)+1:(i + 12)));
+
+       }
+
+    }
+
+
+}
