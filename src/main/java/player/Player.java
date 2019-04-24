7ff4bf449d0320fe710bd34cd4618deaa815ab29
rafiibro <rafael.dacunhagarcia@icloud.com>
2019-04-24 11:42:56 +0200

    Fin iteration 1

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,5 +1,6 @@
 package player;
 
+import board.Board;
 import board.Square;
 import player.piece.Piece;
 import player.piece.TypePiece;
@@ -7,11 +8,15 @@ import player.piece.TypePiece;
 public class Player {
     private String name;
     private Piece piece;
+    private Board board;
 
-    public Player(String name, TypePiece type, Square location) {
+    public Player(String name, TypePiece type, Square location, Board board) {
+        this.board = board;
         this.name = name;
         this.piece = new Piece(type, location);
     }
+
+
     public String getName() {
         return name;
     }
@@ -22,5 +27,9 @@ public class Player {
 
     public String toString() {
         return name + " " + piece;
+    }
+
+    public void takeTurn(int faceValue) {
+        piece.setNewLocation(board.getSquare(piece.getLocation(), faceValue));
     }
 }
